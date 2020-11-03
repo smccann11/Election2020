d48c73d2997b23a826258c50bbc761e9a7a6b8d2
smccann11 <70291882+smccann11@users.noreply.github.com>
2020-11-02 22:03:56 -0800

    Update HW03.py

--- HW03.py
+++ HW03.py
@@ -2,8 +2,10 @@ import praw
 import random
 import datetime
 from textblob import TextBlob
+import time
 
-import random
+
+
 def generate_comment_0():
     adjs = ['good', 'great', 'amazing', 'impactful', 'better']
     adj = random.choice(adjs)
@@ -13,9 +15,10 @@ def generate_comment_0():
     facts = random.choice(factss)
     aspects = ['certain aspects of life', 'science', 'the unkown', 'some phenomena']
     aspect = random.choice(aspects)
-    
+
     text = "Joe Biden will be a " + adj + " president becasue he will listen to " + expert + " and implement policy based on " + facts + " not feeling. " + expert + " know best, as they have devoted their lives to advancing knowledge of " + aspect + "."
     return text
+
 
 def generate_comment_1():
     advs = ['fight', 'advocate', 'push', 'champion']
@@ -28,9 +31,10 @@ def generate_comment_1():
     good = random.choice(goods)
     peoples = ['americans', 'the people', 'citizens', 'us']
     people = random.choice(peoples)
-    
+
     text = "Biden and Harris will " + adv + " for the natural human rights of all people. Any " + adj + " candidate would. An example of a natural human right would be " + right + ". Voting for Biden would be voting for the " + good + " of " + people + "."
     return text
+
 
 def generate_comment_2():
     works = ['work', 'policy', 'ideas', 'achievements']
@@ -43,9 +47,10 @@ def generate_comment_2():
     person = random.choice(persons)
     others = ['americans', 'the people', 'citizens', 'everyone']
     other = random.choice(others)
-    
+
     text = "Joe Biden's " + work + " under the Obama administration " + show + " his passion for the American people. Biden was a major part in revamping the auto industry, and " + fight + " for people. " + person + " vote for Biden is a vote for " + other + "."
     return text
+
 
 def generate_comment_3():
     works = ['work', 'policy', 'ideas', 'actions']
@@ -58,9 +63,10 @@ def generate_comment_3():
     person = random.choice(persons)
     others = ['profanity', 'hate', 'racism', 'ignorance']
     other = random.choice(others)
-    
+
     text = "Donald Trump's " + work + " under his administration " + show + " his ignorance for the American people. Trump ignored the severity of COVID-19, and never " + fight + " for american people. " + person + " vote for Trump is a vote for " + other + "."
     return text
+
 
 def generate_comment_4():
     works = ['work', 'policy', 'ideas', 'actions']
@@ -73,9 +79,10 @@ def generate_comment_4():
     person = random.choice(persons)
     values = ['values', 'ideas', 'thoughts', 'actions']
     value = random.choice(values)
-    
+
     text = "If you believe the " + work + " of Doanald Trump under his administration " + show + " the best of the American people, re-examine your thoughts. His ignorance for the American people, although maybe not obvious is" + eve + ". Trump ignored the severity of COVID-19. " + person + " vote should align with your best" + value + "."
     return text
+
 
 def generate_comment_5():
     works = ['words', 'tweets', 'ideas', 'actions']
@@ -88,129 +95,152 @@ def generate_comment_5():
     person = random.choice(persons)
     values = ['health', 'safety', 'equality', 'hope']
     value = random.choice(values)
-    
-    text = "Trump's " + work + show + "  the person he is. An ignorant, selfish, and stobborn man without sufficient knowledge of skills to be a president. It is " + eve + " that Trump does not care about the average person, but rather himself and his reputaiton. " + person + " vote could end this era, and begin one full of " + value + "."
+
+    text = "Trump's " + work + " " + show + " the person he is. An ignorant, selfish, and stobborn man without sufficient knowledge or skills to be a president. It is " + eve + " that Trump does not care about the average person, but rather himself and his reputaiton. " + person + " vote could end this era, and begin one full of " + value + "."
     return text
+
 
 def generate_comment():
     for i in range(1):
-        options = [0,1,2,3,4,5]
+        options = [0, 1, 2, 3, 4, 5]
         choice = random.choice(options)
         if choice == 0:
-            print(generate_comment_0())
+            return generate_comment_0()
         elif choice == 1:
-            print(generate_comment_1())
+            return generate_comment_1()
         elif choice == 2:
-            print(generate_comment_2())
+            return generate_comment_2()
         elif choice == 3:
-            print(generate_comment_3())
+            return generate_comment_3()
         elif choice == 4:
-            print(generate_comment_4())
+            return generate_comment_4()
         elif choice == 5:
-            print(generate_comment_5())
-generate_comment()
+            return generate_comment_5()
 
-reddit = praw.Reddit('cs40botspen')
 
-reddit_debate_url = 'https://www.reddit.com/r/csci040temp/comments/jhb20w/2020_debate_thread/'
+reddit = praw.Reddit('labcs40bot1')
+
+reddit_debate_url = 'https://www.reddit.com/r/csci040temp/comments/jmzuxh/rudy_giuliani_is_my_father_please_everyone_vote/'
 submission = reddit.submission(url=reddit_debate_url)
 
-while True:
+if True:
 
     print()
-    submission.comments.replace_more(limit=1)
-    print('new iteration at:',datetime.datetime.now())
-    print('submission.title=',submission.title)
-    print('submission.url=',submission.url)
+    submission.comments.replace_more(limit=2)
+    print('new iteration at:', datetime.datetime.now())
+    print('submission.title=', submission.title)
+    print('submission.url=', submission.url)
 
-    # FIXME (task 0): get a list of all of the comments in the submission
-    # HINT: this requires using the .list() and the .replace_more() functions
     all_comments = []
-    
+
     for comment in submission.comments.list():
         all_comments.append(comment)
-        
-    print('len(all_comments)=',len(all_comments))
 
-    # FIXME (task 1): filter all_comments to remove comments that were generated by your bot
-    # HINT: 
-    # use a for loop to loop over each comment in all_comments,
-    # and an if statement to check whether the comment is authored by you or not
+    print('len(all_comments)=', len(all_comments))
+
     not_my_comments = []
 
-    for comment in submission.comment.list():
-        if comment.author=='cs40botspen':
+    for comment in submission.comments.list():
+        if comment.author != 'cs40botspen':
             not_my_comments.append(comment)
 
-    print('len(not_my_comments)=',len(not_my_comments))
+    print('len(not_my_comments)=', len(not_my_comments))
 
-    # if the length of your all_comments and not_my_comments lists are the same,
-    # then that means you have not posted any comments in the current submission;
-    # (you bot may have posted comments in other submissions);
-    # your bot will behave differently depending on whether it's posted a comment or not
     has_not_commented = len(not_my_comments) == len(all_comments)
 
     if has_not_commented:
-        # FIXME (task 2)
-        # if you have not made any comment in the thread, then post a top level comment
-        #
-        # HINT:
-        # use the generate_comment() function to create the text,
-        # and the .reply() function to post it to reddit
-
+        s = generate_comment()
+        submission.reply(s)
     else:
-        # FIXME (task 3): filter the not_my_comments list to also remove comments that 
-        # you've already replied to
-        # HINT:
-        # there are many ways to accomplish this, but my solution uses two nested for loops
-        # the outer for loop loops over not_my_comments,
-        # and the inner for loop loops over all the replies of the current comment from the outer loop,
-        # and then an if statement checks whether the comment is authored by you or not
+
+        # FIXME (task 3): filter the not_my_comments list to also remove comments that
+
         comments_without_replies = []
-        # HINT:
-        # this is the most difficult of the tasks,
-        # and so you will have to be careful to check that this code is in fact working correctly
-        print('len(comments_without_replies)=',len(comments_without_replies))
+        for comment in not_my_comments:
+            if comment.author != 'cs40botspen':
+                response = False
+                for reply in comment.replies:
+                    if str(reply.author) == 'cs40botspen':
+                        response = True
+                if response == False:
+                    comments_without_replies.append(comment)
+        print('len(comments_without_replies)=', len(comments_without_replies))
 
         # FIXME (task 4): randomly select a comment from the comments_without_replies list,
-        # and reply to that comment
-        #
-        # HINT:
-        # use the generate_comment() function to create the text,
-        # and the .reply() function to post it to reddit
 
-    # FIXME (task 5): select a new submission for the next iteration;
-    # your newly selected submission should have a 50% chance of being the original submission
-    # (url in the reddit_debate_url variable)
-    # and a 50% chance of being randomly selected from the top submissions to the csci040 subreddit for the past month
-    # HINT: 
-    # use random.random() for the 50% chance,
-    # if the result is less than 0.5,
-    # then create a submission just like is done at the top of this page;
-    # otherwise, create a subreddit instance for the csci40 subreddit,
-    # use the .top() command with appropriate parameters to get the list of all submissions,
-    # then use random.choice to select one of the submissions
+        for comments in comments_without_replies:
+            choice = random.choice(comments_without_replies)
+            myreply = generate_comment()
+            try:
+                choice.reply(myreply)
+            except praw.exceptions.RedditAPIException as error:
+                if "DELETED_COMMENT" in str(error):
+                    print("Comment" + comment.id + "was deleted")
+                else:
+                    print(error)
+            except Exception as e:
+                print('rate limit exception', e)
+                time.sleep(60 * 10)
 
-     
-    for comemnt in all_comments:
+        # FIXME (task 5): select a new submission for the next iteration;
 
-        blob = textblob(comment)
-        polarity = blob.sentiment.polarity
-        subjectivity = blob.sentiment.subjectivity
-
-        print('comment=',comment)
-        print('polarity=',polarity)
-        print('subjectivity=',subjectivity)
+        randomsub = reddit.subreddit('csci040temp').random()
+        choices = [randomsub, submission]
+        number = random.randint(0,101)
+        if number < 50:
+            submission = randomsub
+            print('Random Sub')
+        else:
+            submission = reddit.submission(url=reddit_debate_url)
+            print('Original Sub')
         print()
 
-    if 'Biden' in comment and polarity > 0: #upvote
+    #sentiment analysis
 
-    if 'Biden' in comment and polarity < 0: #downvote
+    for comment in submission.comments.list():
+        blob = TextBlob(comment.body)
+        polarity = blob.sentiment.polarity
 
-    if 'Trump' in comment and polarity > 0: #upvote
+        print('comment=', comment)
+        print('polarity=', polarity)
+        print()
 
-    if 'Trump' in comment and polarity > 0: #downvote
-    
+    for comment in submission.comments.list():
+        blob = TextBlob(comment.body)
+        polarity = blob.sentiment.polarity
+        if 'Biden' in comment.body and polarity > 0:
+            comment.upvote()
+            print('comment upvoted!')
+        if 'Biden' in comment.body and polarity < 0:
+            comment.downvote()
+            print('comment downvoted!')
+        if 'Trump' in comment.body and polarity > 0:
+            comment.upvote()
+        print('comment upvoted!')
+        if 'Trump' in comment.body and polarity > 0:
+            comment.downvote()
+            print('comment downvoted!')
 
-    
-    
+    for submission in reddit.subreddit('csci040temp'):
+        blob = TextBlob(submission.title)
+        polarity = blob.sentiment.polarity
+
+        print('submission=', comment)
+        print('polarity=', polarity)
+        print()
+
+    for submission in reddit.subreddit('csci040temp'):
+        blob = TextBlob(submission.title)
+        polarity = blob.sentiment.polarity
+        if 'Biden' in submission.title and polarity > 0:
+            submission.upvote()
+            print('submission upvoted!')
+        if 'Biden' in submission.title and polarity < 0:
+            submission.downvote()
+            print('submission downvoted!')
+        if 'Trump' in submission.title and polarity > 0:
+            submission.upvote()
+            print('submission upvoted!')
+        if 'Trump' in submission.title and polarity > 0:
+            submission.downvote()
+            print('submission downvoted!')
